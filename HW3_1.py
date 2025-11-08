51bc1933ec63c43ee64428cce0babc60ce33660f
nnabokus <loverockmusik@gmail.com>
2025-11-08 18:12:25 +0200

    Update HW3_1.py

--- HW3_1.py
+++ HW3_1.py
@@ -1,1 +1,16 @@
-
+a = int(input("Введіть перше число:"))
+b = int(input("Введіть друге число:"))
+action = input("Введіть дію (+, -, *, /):")
+if action == '+':
+    print("Результат:", a + b)
+elif action == '-':
+    print("Результат:", a - b)
+elif action == '*':
+    print("Результат:", a * b)
+elif action == '/':
+    if b != 0:
+        print("Результат:", a / b)
+    else:
+        print("Неможливо поділити на 0!")
+else:
+    print("Невідома дія")
