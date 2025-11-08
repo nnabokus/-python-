df17266c64a063a1c207c53083174ea405efcc90
nnabokus <loverockmusik@gmail.com>
2025-11-08 18:48:40 +0200

    Create HW3_3.py

---
+++ HW3_3.py
@@ -0,0 +1,24 @@
+# Приклад 1
+lst = [1, 2, 3, 4, 5, 6]
+mid = (len(lst) + 1) // 2
+print([lst[:mid], lst[mid:]])
+
+# Приклад 2
+lst = [1, 2, 3]
+mid = (len(lst) + 1) // 2
+print([lst[:mid], lst[mid:]])
+
+# Приклад 3
+lst = [1, 2, 3, 4, 5]
+mid = (len(lst) + 1) // 2
+print([lst[:mid], lst[mid:]])
+
+# Приклад 4
+lst = [1]
+mid = (len(lst) + 1) // 2
+print([lst[:mid], lst[mid:]])
+
+# Приклад 5
+lst = []
+mid = (len(lst) + 1) // 2
+print([lst[:mid], lst[mid:]])
