ddac63eff24728220dfe5ea36a7ea8546c90d8b5
nnabokus <loverockmusik@gmail.com>
2025-12-02 05:32:27 +0200

    Delete HW9_1.py

--- HW9_1.py
+++
@@ -1,9 +0,0 @@
-def popular_words(text, words):
-    text = text.lower()
-    text_words = text.split()
-    
-    result = {}
-    for w in words:
-        result[w] = text_words.count(w)
-    
-    return result
