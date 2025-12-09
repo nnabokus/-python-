ec53697ba7e85dd41de12c6aac225699b37ad921
nnabokus <loverockmusik@gmail.com>
2025-12-09 07:06:29 +0200

    Create HW11_2.py

---
+++ HW11_2.py
@@ -0,0 +1,17 @@
+def generate_cube_numbers(end):
+    num = 2
+    while True:
+        cube = num ** 3
+        if cube > end:
+            return
+        yield cube
+        num += 1
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8],
+assert list(generate_cube_numbers(100)) == [8, 27, 64],
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000],
+print('Ok')
